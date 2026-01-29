7f2ffb01dcc49e49f679c54943e3f92cc36c887d
Yosshy <Yosshy_123@proton.me>
2026-01-29 15:13:04 +0900

    Update hop.js

--- hop.js
+++ hop.js
@@ -1,4 +1,18 @@
-app.use((req, res, next) => {
-  console.log(req.headers['x-forwarded-for']);
-  next();
+// hop-check.js
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const xff = req.headers['x-forwarded-for'];
+
+  res.json({
+    'x-forwarded-for': xff,
+    hops: xff ? xff.split(',').map(v => v.trim()).length : 0,
+    rawHeaders: req.headers
+  });
+});
+
+app.listen(3000, () => {
+  console.log('hop check server listening on 3000');
 });
